a6da0d59f72740ee2bad137fe7c4cbad9670b1c2
alvaroluisv <alvaroluisvasquez@gmail.com>
2021-01-26 21:33:17 -0500

    Fin seccion 15

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,96 +1,101 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import Swal from 'sweetalert';
-// import * as Swal from 'sweetalert'
+import Swal from 'sweetalert'
 
 import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent {
+
   public formSubmitted = false;
 
-  public registerForm = this.fb.group(
-    {
-      nombre: ['Alvaro', Validators.required],
-      email: ['test100@gmail.com', [Validators.required, Validators.email]],
-      password: ['123456', Validators.required],
-      password2: ['123456', Validators.required],
-      terminos: [true, Validators.required],
-    },
-    {
-      validators: this.passwordsIguales('password', 'password2'),
-    }
-  );
+  public registerForm = this.fb.group({
+    nombre: ['Fernando', Validators.required ],
+    email: ['test100@gmail.com', [ Validators.required, Validators.email ] ],
+    password: ['123456', Validators.required ],
+    password2: ['123456', Validators.required ],
+    terminos: [ true, Validators.required ],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
+  });
 
-  constructor(
-    private fb: FormBuilder,
-    private usuarioService: UsuarioService,
-    private router: Router
-  ) {}
+  constructor( private fb: FormBuilder,
+               private usuarioService: UsuarioService,
+               private router: Router ) { }
 
   crearUsuario() {
     this.formSubmitted = true;
-    // console.log(this.registerForm.value);
+    console.log( this.registerForm.value );
 
-    if (this.registerForm.invalid) {
+    if ( this.registerForm.invalid ) {
       return;
     }
 
     // Realizar el posteo
-    this.usuarioService.crearUsuario(this.registerForm.value).subscribe(
-      (resp) => {
-        // Navegar al Dashboard
-        this.router.navigateByUrl('/');
-      },
-      (err) => {
-        // Si sucede un error
-        // Swal.error('Error', err.error.msg, 'error' );
-        Swal({
-          title: err.error.msg,
-          icon: 'error',
+    this.usuarioService.crearUsuario( this.registerForm.value )
+        .subscribe( resp => {
+          
+          // Navegar al Dashboard
+          this.router.navigateByUrl('/');
+
+        }, (err) => {
+          // Si sucede un error
+          Swal({
+            title: 'Error', 
+            text: err.error.msg, 
+            icon: 'error'
+          } );
         });
-      }
-    );
+
+
   }
 
-  campoNoValido(campo: string): boolean {
-    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+  campoNoValido( campo: string ): boolean {
+    
+    if ( this.registerForm.get(campo).invalid && this.formSubmitted ) {
       return true;
     } else {
       return false;
     }
+
   }
 
   contrasenasNoValidas() {
     const pass1 = this.registerForm.get('password').value;
     const pass2 = this.registerForm.get('password2').value;
 
-    if (pass1 !== pass2 && this.formSubmitted) {
+    if ( (pass1 !== pass2) && this.formSubmitted ) {
       return true;
     } else {
       return false;
     }
+
   }
 
   aceptaTerminos() {
     return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 
-  passwordsIguales(pass1Name: string, pass2Name: string) {
-    return (formGroup: FormGroup) => {
+  passwordsIguales(pass1Name: string, pass2Name: string ) {
+
+    return ( formGroup: FormGroup ) => {
+
       const pass1Control = formGroup.get(pass1Name);
       const pass2Control = formGroup.get(pass2Name);
 
-      if (pass1Control.value === pass2Control.value) {
-        pass2Control.setErrors(null);
+      if ( pass1Control.value === pass2Control.value ) {
+        pass2Control.setErrors(null)
       } else {
-        pass2Control.setErrors({ noEsIgual: true });
+        pass2Control.setErrors({ noEsIgual: true })
       }
-    };
+
+
+    }
   }
+
 }
